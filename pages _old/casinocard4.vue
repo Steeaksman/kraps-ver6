5093282e0798982a0aa0e45cc0d6690923db24a4
Donny <nstokes142@gmail.com>
2025-01-27 20:37:05 -0800

    upgraded node version

--- pages _old/casinocard4.vue
+++ pages _old/casinocard4.vue
@@ -16,7 +16,7 @@
   <div class="profile flex items-center even:flex-row-reverse even:text-right">
    
     <div>
-        <h1 class="text-2xl">Wind Creek Casino and Hotel</h1>
+        <h1 class="text-2xl">Wind Creek Casino and haunted Hotel</h1>
         <h1>Atmore, Alabama</h1>
         <h1>Website: BumfuckCasino@backwoods.org</h1>
         <h1>Has Craps: Yes! </h1>
